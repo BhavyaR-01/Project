6d7d153430ebe012f0cc30cbb521a9e176cc8a9f
BhavyaR-01 <144333670+BhavyaR-01@users.noreply.github.com>
2023-09-16 12:20:26 +0530

    first commit

---
+++ CustomerServices/src/main/java/com/cg/cs/CustomerServices.java
@@ -0,0 +1,40 @@
+package com.cg.cs;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CustomerServices {
+	
+	@Autowired
+ private CustomerRepository repository;
+	
+	public List<Customer>listAll(){
+		return repository.findAll();
+		
+				}
+	
+	public Customer get(Integer id) {
+		return repository.findById(id).get();
+		
+	}
+	//create
+	public void save(Customer customer) {
+		repository.save(customer);
+		}
+	
+	//detete
+	
+	public void delete(Integer id)
+	{
+		repository.deleteById(id);
+		
+		
+	}
+	
+	
+}
